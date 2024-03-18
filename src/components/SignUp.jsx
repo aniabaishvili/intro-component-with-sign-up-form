f0172109e51c1524154915d881f23da9550b7b95
ani <aniabaishvili1@gmail.com>
2024-03-18 22:55:48 +0400

    finish signup form

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,12 +7,12 @@ export default function SignUp() {
         Try it free 7 days <span>then $20/mo. thereafter</span>
       </Price>
       <SignForm>
-       
-        <Input placeholder="First name"/>
-        <Input placeholder="Last name"/>
-        <Input placeholder="Email Address"/>
-        <Input placeholder="Password"/>
-        
+        <Input placeholder="First name" />
+        <Input placeholder="Last name" />
+        <Input placeholder="Email Address" />
+        <Input placeholder="Password" />
+        <Button>CLAIM YOUR FREE TRIAL</Button>
+        <Footer>By clicking the button, you are agreeing to our <span>Terms and Services</span></Footer>
       </SignForm>
     </SignStyles>
   );
@@ -24,6 +24,12 @@ const SignForm = styled.div`
   box-shadow: 0px 8px 0px 0px #00000025;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  border-radius: 10px;
+  row-gap: 10px;
+  margin-top: 30px;
 `;
 
 const Price = styled.h2`
@@ -38,6 +44,7 @@ const Price = styled.h2`
   padding: 10px 50px;
   text-align: center;
   margin-top: 20px;
+  border-radius: 10px;
 
   & span {
     font-weight: 300;
@@ -49,4 +56,34 @@ const Price = styled.h2`
 `;
 export const SignStyles = styled.div``;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  width: 279px;
+  padding: 20px;
+  border: 1px solid #dedede;
+`;
+
+export const Button = styled.button`
+  background: #38cc8b;
+  box-shadow: 0px -4px 0px 0px #00000017 inset;
+  padding: 18px;
+  color: #fff;
+  width: 279px;
+  border-style: none;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 26px;
+  border-radius: 10px;
+`;
+
+export const Footer = styled.footer`
+    font-size: 11px;
+    font-weight: 600;
+    line-height: 21px;
+    letter-spacing: 0px;
+    text-align: center;
+    color:#dedede;
+    & span {
+        color: #FF7979;
+    }
+
+`
